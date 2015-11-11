4fd56621466fab439d9ef7afe10f9280b618fc33
20141105048 <20141105048@sina.com>
2015-11-11 18:55:33 +0800

    Initial Commit

---
+++ ss/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ss
+//
+//  Created by 20141105048 on 15/11/11.
+//  Copyright © 2015年 20141105048. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
